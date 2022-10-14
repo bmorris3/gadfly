451cb8948db0423316669ccbbb2ac0656ee81e8d
Brett M. Morris <bmmorris@stsci.edu>
2022-10-13 21:27:07 -0400

    revisiting the kernel fitting problem

--- gadfly/psd.py
+++ gadfly/psd.py
@@ -1,9 +1,10 @@
 import numpy as np
+import astropy.units as u
 
 __all__ = ['power_spectrum']
 
 
-def power_spectrum(fluxes, d=60):
+def power_spectrum(flux, d=60 * u.s):
     """
     Compute the power spectrum of ``fluxes`` in units of [ppm^2 / microHz].
 
@@ -11,17 +12,27 @@ def power_spectrum(fluxes, d=60):
     ----------
     fluxes : ~numpy.ndarray
         Fluxes with zero mean.
-    d : float
-        Time between samples [s].
+    d : ~astropy.units.Quantity
+        Time between samples.
 
     Returns
     -------
     freq : numpy.ndarray
-        Frequencies
+        Frequencies in microHz
     power : numpy.ndarray
         Power at each frequency in units of [ppm^2 / microHz]
     """
-    fft = np.fft.rfft(1e6 * fluxes)
-    power = (fft * np.conj(fft)).real * 1e-6 / len(fluxes)
-    freq = np.fft.rfftfreq(len(fluxes), d)
-    return freq, power
+    if hasattr(flux, 'unit'):
+        # if flux has units, use them!
+        flux_unit = flux.unit
+        flux_unscaled = flux.to(u.dimensionless_unscaled).value
+    else:
+        # otherwise, assume fluxes are not rescaled
+        flux_unit = u.dimensionless_unscaled
+        flux_unscaled = flux
+
+    fft = np.fft.rfft(flux_unscaled, norm="backward")
+    power = np.real(fft * np.conj(fft)) * d.to(1 / u.uHz) / len(flux_unscaled)
+    freq = np.fft.rfftfreq(len(flux_unscaled), d.to(1 / u.uHz))
+
+    return freq, power.to(flux_unit ** 2 / u.uHz)
